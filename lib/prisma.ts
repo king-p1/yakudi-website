fc50bc6363d2b20841237fc6d370d89282964680
king-p1 <oladelex4@gmail.com>
2026-05-15 01:42:39 -0400

    bug fixes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -18,7 +18,7 @@
 
 import "dotenv/config";
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = process.env.DATABASE_URL;
 const adapter = new PrismaNeon({ connectionString });
